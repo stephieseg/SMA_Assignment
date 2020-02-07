1b2fb73ec63f2283d4cd18acb7f97b087722097a
stephieseg <54999530+stephieseg@users.noreply.github.com>
2020-02-07 07:21:07 +0100

    Update 01_processing_tweets.R

--- 01_processing_tweets.R
+++ 01_processing_tweets.R
@@ -1,6 +1,6 @@
 #--------------------------------Social Media Analytics-------------------------------#
 ####                                                                               ####
-####   This file reads is used to download tweets and save them as RData files     ####
+####   This file is used to download tweets and save them as RData files           ####
 ####  Team members: Stephanie Beyer Diaz, Manoj Purushothaman, Edward Vrighjem     ####
 ####                                 Company: Sprint                               ####
 ####                                                                               ####
@@ -20,8 +20,10 @@ twitter_token <- create_token(
   access_secret = access_secret,
   set_renv=FALSE)
 
+#Sprint handles
 handles <- c("@Sprint","@sprintcare","@sprintnews","@sprintbusiness")
 
+#Sprint keywords
 term_list <- c("#MySprintRewards",
               "sprint.com",
               "Sprint Store",#%20
@@ -237,11 +239,7 @@ tweets_df$state <- ifelse(trimws(tweets_df$location) %in% uscities$city_ascii,le
                            ifelse(uscities$state_id %in% toupper(tweets_df$location),levels(uscities$state_name),
                                   ifelse(toupper(uscities$state_name) %in% toupper(tweets_df$location),levels(uscities$state_name),NA)))))))))))))
 
-#saving dataframes in case more tweets are downloaded later
-save(followers,file="followers.RData")
-save(corp_accounts,file="corp_accounts.RData")
-save(tweets_df,file="tweets_df.RData")
-
+#PROCESSING FILES USED AS INPUT IN SHINY
 #saving dictionaries
 afinn <- get_sentiments(lexicon = "afinn") %>% rename("afinn"="value")
 bing <- get_sentiments(lexicon = "bing") %>% rename("bing"="sentiment")
@@ -254,3 +252,227 @@ save(bing,file="bing.RData")
 save(loughran,file="loughran.RData")
 save(nrc,file="nrc.RData")
 save(syuzhet,file="syuzhet.RData")
+
+
+#--------------------Functions for Cleaning and Preprocessing Tweets------------------#
+
+#Creating function to clean text column, using a hybrid aproach
+clean_text <- function(data,col,output) {
+  ## removing urls
+  data <- mutate(data, clean = gsub(x = col, pattern = "(s?)(f|ht)tp(s?)://\\S+\\b", replacement = ""))
+  
+  ## removing new lines
+  data <- mutate(data, clean = gsub(x = clean, pattern = "[\r\n]", replacement = " "))
+  
+  ## removing hashtags and mentions
+  data <- mutate(data, clean = gsub(x = clean, pattern = "#|@", replacement = " "))
+  
+  ## removing apostrophes separately to keep string as one word
+  data <- mutate(data, clean = gsub(x = clean, pattern = "'|'", replacement = ""))
+  
+  ## removing punctuation manually to add a space and avoid merging words
+  data <- mutate(data, clean = gsub(x = clean, pattern = "[0-9]+|[[:punct:]]|\\(.*\\)", replacement = " "))
+  
+  ## removing emojis
+  data$clean <- sapply(data$clean,function(row) iconv(row, "latin1", "ASCII", sub=" "))
+  
+  ## final column name
+  names(data)[names(data) == 'clean'] <- output
+  
+  return(data)
+}
+
+
+#Creating function for creating a corpus, to be used after cleaning function
+tw_corpus <- function(data, col){
+  
+  ## build a corpus, and specify the source to be character vectors
+  myCorpus <- Corpus(VectorSource(col))
+  
+  ## convert to lower case
+  myCorpus <- tm_map(myCorpus, content_transformer(tolower))
+  
+  ## removing numbers as they lose meaning
+  myCorpus <- tm_map(myCorpus, removeNumbers)
+  
+  ## remove stopwords from corpus
+  stoppers <- stopwords("english")
+  stoppers <- append(stoppers,c("https","com","can","http"))
+  myCorpus <- tm_map(myCorpus, removeWords, stoppers)
+  
+  ## adding metadata, suppressing warning in case columns are not available
+  suppressWarnings(meta(myCorpus, 'author') <- data$screen_name)
+  suppressWarnings(meta(myCorpus, 'datetimestamp') <- data$created_at)
+  suppressWarnings(meta(myCorpus, 'description') <- data$description)
+  suppressWarnings(meta(myCorpus, 'origin') <- data$source)
+  suppressWarnings(meta(myCorpus, 'location') <- data$location)
+  
+  return(myCorpus)
+}
+
+
+# Tokenization function, with stemming, lemmatization and adding custom stopwords optional
+# requires clean_tweet column
+tokenize <- function(data,stem=FALSE,lemma=TRUE,custom=c(),dtm=TRUE){
+  stoppers <- get_stopwords()
+  if (!is.null(custom)){
+    for (cust in custom){
+      stoppers <- rbind(stoppers,c(cust,'custom'))}
+  }
+  tokenized <- data %>% unnest_tokens(output = "word", 
+                                      input = clean_tweet, 
+                                      token = "words", 
+                                      drop=FALSE,to_lower=TRUE) %>%
+    anti_join(stoppers) %>%
+    mutate(word_stem = wordStem(word)) 
+  
+  if (stem==FALSE){ #stemming not always necessary
+    tokenized$word_stem <- NULL}
+  
+  else {
+    tokenized$word <- tokenized$word_stem
+    tokenized$word_stem <- NULL}
+  
+  if (lemma==TRUE){
+    tokenized$word <- lemmatize_words(tokenized$word)
+  }
+  
+  
+  if (dtm==TRUE) {
+    tokenized <- tokenized %>%
+      count(status_id,word , sort=TRUE) %>%
+      cast_dtm(document = status_id, term = word,
+               value = n, weighting = tm::weightTf)
+  }
+  return(tokenized)
+}
+
+#-----------------------Creating a Corpus for each set of tweets----------------------#
+
+## Applying cleaning and corpus functions for each dataframe
+followers <- clean_text(followers,followers$tweet,"clean_tweet")
+followers_corpus <- tw_corpus(followers, followers$clean_tweet)
+
+corp_accounts <- clean_text(corp_accounts,corp_accounts$tweet,"clean_tweet")
+corp_accounts_corpus <- tw_corpus(corp_accounts, corp_accounts$clean_tweet)
+
+tweets_df <- clean_text(tweets_df, tweets_df$tweet,"clean_tweet")
+tweets_df_corpus <- tw_corpus(tweets_df, tweets_df$clean_tweet)
+
+#Creating df with tweets excluding tweets by Sprint accounts
+user_tweets <- tweets_df[!tweets_df$screen_name %in% handles,] 
+
+#Creating dataframe with mentions of COMPETITOR BRANDS for later use
+competition <- c("verizon","tmobile","ATT","xfinity","comcast")
+competition_df <- c()
+for (i in competition){
+  #getting tweets mentioning the competition
+  prep_df <- user_tweets[grep(i, user_tweets$mentions_screen_name),] 
+  competition_df <- rbind(competition_df, prep_df)
+}
+
+#tokenizing tweets with additional custom stopwords
+tweetsTokenized <- tokenize(user_tweets,custom=c('sprint','can','get','t','amp','s','im','cant'))
+k <- seq(2,10)
+
+#running LDA model for different amount of topics
+lda_df <- c()
+for (each in k) {
+  print(paste("each",each))
+  tweets_lda <- LDA(tweetsTokenized, k = each, method="gibbs")
+
+  #getting first 3 words in topic
+  term <- terms(tweets_lda, 3) 
+  term <- apply(term, MARGIN = 2, paste, collapse = ", ")
+  topic <- topics(tweets_lda, 1)
+  print(term)
+  #preparing data frame to plot topics, filtering tweets not in docs to use date as axis
+  docs <- names(topic)
+  tw_x <- user_tweets[user_tweets$status_id %in% docs, "created_at"]
+  topics <- data.frame(date=as.IDate(tw_x$created_at), topic) 
+  topics <- cbind(k=rep(k,length.out=nrow(topics)),topics)
+    topics$term1 <- term[1]
+    topics$term2 <- term[2]
+    topics$term3 <- term[3]
+    topics$term4 <- term[4]
+    topics$term5 <- term[5]
+    topics$term6 <- term[6]
+    topics$term7 <- term[7]
+    topics$term8 <- term[8]
+    topics$term9 <- term[9]
+    topics$term10 <- term[10]
+  lda_df <- rbind(lda_df,topics)
+  }
+
+#saving lda output for later use
+save(lda_df,file="lda_df.RData")
+
+user_tweets <- clean_text(user_tweets,user_tweets$description,"clean_desc")
+
+#engagement function for future use
+engagement <- function(df){
+  df$engagement <- (df$followers_count/df$statuses_count) +
+  (df$favourites_count/df$statuses_count) +
+  (df$retweet_count/df$statuses_count)
+  return(df)
+}
+
+user_tweets <- engagement(user_tweets)
+corp_accounts <- engagement(corp_accounts)
+
+
+#saving dataframes in case more tweets are downloaded later
+save(followers,file="followers.RData")
+save(corp_accounts,file="corp_accounts.RData")
+save(tweets_df,file="tweets_df.RData")
+save(user_tweets,file="user_tweets.RData")
+
+#frequency table for wordclouds
+follow_buzz <- clean_text(tweets_df,tweets_df$tweet,"clean_desc")
+follow_buzz <- tw_corpus(follow_buzz, follow_buzz$clean_desc)
+tdm<-TermDocumentMatrix(follow_buzz)
+tdmatrix<-as.matrix(tdm)
+wordfreq<-sort(rowSums(tdmatrix), decreasing = TRUE)
+freqtable <- as.data.frame(wordfreq)
+freqtable$word <- row.names(freqtable)
+rownames(freqtable)<- NULL
+
+save(freqtable,file="freqtable.RData")
+
+#getting relevant columns and cleaning hashtag column
+hashtag_df <- user_tweets[,c("status_id","hashtags","created_at","clean_tweet","engagement")]
+hashtag_df$hashtags <- ifelse(hashtag_df$hashtags=="NA","",hashtag_df$hashtags)
+
+#adding tweet sentiment columns
+tweetSentiment <- left_join(hashtags,afinn)
+tweetSentiment <- left_join(tweetSentiment,bing)
+tweetSentiment <- left_join(tweetSentiment,loughran)
+tweetSentiment <- left_join(tweetSentiment,nrc)
+tweetSentiment <- left_join(tweetSentiment,syuzhet)
+
+#getting distinct hashtags: to see what sentiments are associated with top hashtags
+hashtag_dist <- tweetSentiment
+hashtag_dist$clean_tweet <- hashtag_dist$hashtags
+hashtag_dist$hashtags<- NULL
+hashtag_dist <- tokenize(hashtag_dist,dtm=FALSE)
+
+#top5 hashtags and some sentiment information
+hashtag_top <- hashtag_dist %>% group_by(word) %>% transmute(count=n_distinct(status_id),
+                                                             afinn=mean(afinn[!is.na(afinn)]),
+                                                             syuzhet=mean(syuzhet[!is.na(syuzhet)]),
+                                                             bing_pos=n_distinct(status_id[bing=='positive']),
+                                                             bing_neg=n_distinct(status_id[bing=='negative'])) %>% unique()
+save(hashtag_dist,file="hashtag_dist.RData")
+
+#COMPANY TWEET ANALYSIS (from Sprint account tweets)
+token_prep <- corp_accounts[,c("status_id","hashtags","engagement","created_at","clean_tweet")]
+token_prep$hashtags <- ifelse(token_prep$hashtags=="NA","",token_prep$hashtags)
+
+#tokenizing corporate tweets
+corp_token <- tokenize(token_prep,dtm=FALSE,custom = c("s","us","can","g"))
+corpSentiment <- left_join(corp_token,afinn)
+corpSentiment <- left_join(corpSentiment,bing)
+corpSentiment <- left_join(corpSentiment,loughran)
+corpSentiment <- left_join(corpSentiment,nrc)
+corpSentiment <- left_join(corpSentiment,syuzhet)
+save(hashtag_dist,file="corpSentiment.RData")
